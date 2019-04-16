cfb0d6d41fc30f60c06ab32d1a22a93d6a0f8396
Dominik Zivcic <dominik.zivcic00@gmail.com>
2019-04-16 13:53:01 +0200

    Prva i druga vježba sa izvještajem za drugu vježbu

---
+++ RobotskiVid/RobotskiVid/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace RobotskiVid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CropButton = new System.Windows.Forms.Button();
+            this.CaptureButton = new System.Windows.Forms.Button();
+            this.TemplateButton = new System.Windows.Forms.Button();
+            this.methodBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.threshBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // browse
+            // 
+            this.browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browse.Location = new System.Drawing.Point(765, 12);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(124, 45);
+            this.browse.TabIndex = 3;
+            this.browse.Text = "Ucitaj sliku";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(290, 277);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // CropButton
+            // 
+            this.CropButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CropButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CropButton.Location = new System.Drawing.Point(762, 129);
+            this.CropButton.Name = "CropButton";
+            this.CropButton.Size = new System.Drawing.Size(124, 107);
+            this.CropButton.TabIndex = 5;
+            this.CropButton.Text = "Izrezi i napravi Canny detekciju rubova";
+            this.CropButton.UseVisualStyleBackColor = true;
+            this.CropButton.Click += new System.EventHandler(this.CropButton_Click);
+            // 
+            // CaptureButton
+            // 
+            this.CaptureButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CaptureButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CaptureButton.Location = new System.Drawing.Point(765, 80);
+            this.CaptureButton.Name = "CaptureButton";
+            this.CaptureButton.Size = new System.Drawing.Size(124, 43);
+            this.CaptureButton.TabIndex = 6;
+            this.CaptureButton.Text = "Uslikaj";
+            this.CaptureButton.UseVisualStyleBackColor = true;
+            this.CaptureButton.Click += new System.EventHandler(this.CaptureButton_Click);
+            // 
+            // TemplateButton
+            // 
+            this.TemplateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.TemplateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TemplateButton.Location = new System.Drawing.Point(765, 372);
+            this.TemplateButton.Name = "TemplateButton";
+            this.TemplateButton.Size = new System.Drawing.Size(124, 107);
+            this.TemplateButton.TabIndex = 7;
+            this.TemplateButton.Text = "Izrezi i Template Matching";
+            this.TemplateButton.UseVisualStyleBackColor = true;
+            this.TemplateButton.Click += new System.EventHandler(this.TemplateButton_Click);
+            // 
+            // methodBox
+            // 
+            this.methodBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.methodBox.FormattingEnabled = true;
+            this.methodBox.Items.AddRange(new object[] {
+            "SQDIFF",
+            "SQDIFF_NORMED",
+            "CCORR",
+            "CCORR_NORMED",
+            "CCOEFF",
+            "CCOEFF_NORMED"});
+            this.methodBox.Location = new System.Drawing.Point(765, 334);
+            this.methodBox.Name = "methodBox";
+            this.methodBox.Size = new System.Drawing.Size(121, 21);
+            this.methodBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(700, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Odaberi metodu za Template matching";
+            // 
+            // threshBox
+            // 
+            this.threshBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.threshBox.Location = new System.Drawing.Point(786, 285);
+            this.threshBox.Name = "threshBox";
+            this.threshBox.Size = new System.Drawing.Size(100, 20);
+            this.threshBox.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(835, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Threshold";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 510);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.threshBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.methodBox);
+            this.Controls.Add(this.TemplateButton);
+            this.Controls.Add(this.CaptureButton);
+            this.Controls.Add(this.CropButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.browse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button CropButton;
+        private System.Windows.Forms.Button CaptureButton;
+        private System.Windows.Forms.Button TemplateButton;
+        private System.Windows.Forms.ComboBox methodBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox threshBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RobotskiVid/RobotskiVid/Form1.cs
@@ -0,0 +1,363 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System;
+using System.Drawing;
+
+using System.Windows.Forms;
+
+namespace RobotskiVid
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rect;
+        Rectangle rect_letter;
+        VideoCapture cap = new VideoCapture(0);
+        bool cropFlag = false;
+        bool templateFlag = false;
+        bool draw = false;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            // open file dialog   
+            OpenFileDialog open = new OpenFileDialog();
+            // image filters  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                // display image in picture box  
+                
+                pictureBox1.Image = new Bitmap(open.FileName);
+                // image file path  
+               // textBox1.Text = open.FileName;
+            }
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (cropFlag || templateFlag)
+            {
+                rect = new System.Drawing.Rectangle(e.X, e.Y, 0, 0);
+                this.pictureBox1.Invalidate();
+
+            }
+            
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (cropFlag || templateFlag)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+
+                    rect = new System.Drawing.Rectangle(rect.Left, rect.Top, e.X - rect.Left, e.Y - rect.Top);
+                }
+                this.pictureBox1.Invalidate();
+            }
+          
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+
+        {
+
+            if (cropFlag || templateFlag)
+            {
+              
+                
+                System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Red, 2);
+                e.Graphics.DrawRectangle(pen, rect);
+            }
+
+            if (draw)
+            {
+                System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Green, 2);
+                e.Graphics.DrawRectangle(pen, rect_letter);
+            }
+          
+           
+        }
+
+        private void CropButton_Click(object sender, EventArgs e)
+        {
+            cropFlag = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (cropFlag)
+            {
+                // Create a Bitmap object from a file.
+                Bitmap cropedBitmap = new Bitmap(pictureBox1.Image);
+
+                // Clone a portion of the Bitmap object.
+                //Rectangle cloneRect = new Rectangle(0, 0, 100, 100);
+                System.Drawing.Imaging.PixelFormat format =
+                    cropedBitmap.PixelFormat;
+                Bitmap cloneBitmap = cropedBitmap.Clone(rect, format);
+
+                ImageViewer viewer = new ImageViewer();
+                Image<Bgr, Byte> cvimage = new Image<Bgr, Byte>(cloneBitmap);
+
+                viewer.Image = cvimage;
+                viewer.ShowDialog();
+                // pictureBox2.Image = cropedBitmap;
+                // Draw the cloned portion of the Bitmap object.
+                // e.Graphics.DrawImage(cloneBitmap, 0, 0);
+
+
+                
+       
+
+                //CANNY DETEKTOR
+
+                CvInvoke.UseOpenCL = true;
+               
+                //Bitmap bm = new Bitmap(pictureBox1.Image);
+                Bitmap bm = new Bitmap(cvimage.Bitmap);
+
+
+                Image<Gray, byte> im = new Image<Gray, byte>(bm);
+               // cvimage.ToBitmap();
+               // UMat u = cvimage.ToUMat();
+                UMat u = im.ToUMat();
+
+                CvInvoke.Canny(u, u, 150, 50);
+
+                //pictureBox1.Image = u.Bitmap;
+
+
+                //convert to image
+                //Image<Bgr, Byte> cvimage = new Image<Bgr, Byte>(u.Bitmap);
+                Image<Bgr, byte> outImage = new Image<Bgr, byte>(u.Bitmap);
+
+                viewer.Image = outImage;
+                viewer.ShowDialog();
+
+
+
+                cropFlag = false;
+            }
+
+
+
+            //LV2 template matching
+
+            if(templateFlag )
+            {
+                // Create a Bitmap object from a file.
+                Bitmap cropedBitmap = new Bitmap(pictureBox1.Image);
+
+                // Clone a portion of the Bitmap object.
+                //Rectangle cloneRect = new Rectangle(0, 0, 100, 100);
+                System.Drawing.Imaging.PixelFormat format =
+                    cropedBitmap.PixelFormat;
+                Bitmap cloneBitmap = cropedBitmap.Clone(rect, format);
+
+                ImageViewer viewer = new ImageViewer();
+                Image<Bgr, Byte> templ = new Image<Bgr, Byte>(cloneBitmap);
+
+                viewer.Image = templ;
+                viewer.ShowDialog();
+
+
+                Bitmap image_test = new Bitmap(pictureBox1.Image);
+                Image<Bgr, Byte> myImage = new Image<Bgr, Byte>(image_test);
+                Image<Gray, float> res;
+
+                //template matching metode
+                string selectedItem = methodBox.Items[methodBox.SelectedIndex].ToString();
+
+                if (selectedItem == "SQDIFF")
+                {
+                     res = myImage.MatchTemplate(templ, Emgu.CV.CvEnum.TemplateMatchingType.Sqdiff);
+                }else if(selectedItem == "SQDIFF_NORMED")
+                {
+                     res = myImage.MatchTemplate(templ, Emgu.CV.CvEnum.TemplateMatchingType.SqdiffNormed);
+
+                }
+                else if (selectedItem == "CCORR")
+                {
+                     res = myImage.MatchTemplate(templ, Emgu.CV.CvEnum.TemplateMatchingType.Ccorr);
+
+                }
+                else if (selectedItem == "CCORR_NORMED")
+                {
+                     res = myImage.MatchTemplate(templ, Emgu.CV.CvEnum.TemplateMatchingType.CcorrNormed);
+
+                }
+                else if (selectedItem == "CCOEFF")
+                {
+                     res = myImage.MatchTemplate(templ, Emgu.CV.CvEnum.TemplateMatchingType.Ccoeff);
+
+                }
+                else if (selectedItem == "CCOEFF_NORMED")
+                {
+                     res = myImage.MatchTemplate(templ, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed);
+
+                }
+                else { res = null; }
+                
+           
+
+                // Image<Gray, float> res = myImage.MatchTemplate(templ, Emgu.CV.CvEnum.TemplateMatchingType.);
+                viewer.Image = res;
+                //viewer.ShowDialog();
+
+
+                //cols and rows 
+                int icols = myImage.Cols - templ.Cols + 1;
+                int irows = myImage.Rows - templ.Rows + 1;
+
+                //treshold
+                double thresh =(double)Convert.ToDouble(threshBox.Text);
+               // MessageBox.Show(thresh.ToString());
+
+                int windowSize = 11;
+                int windowRad = (int)(windowSize / 2);
+                float centerPixelValue, currentPixelValue;
+                bool bcenterPixelMax = false;
+
+
+                //double ress = res.Norm;
+                //prolaz kroz sve pixele
+
+                for ( int y =0; y<irows; y++)
+                {
+                    for (int x = 0; x < icols; x++)
+                    {
+                        if ((x - windowRad >= 0) && (x + windowRad <= icols - 1) && (y - windowRad >= 0) && (y + windowRad <= irows - 1))
+                        {
+                            //vrijednost pixela
+                            centerPixelValue = res.Data[y, x, 0];
+                            // MessageBox.Show(centerPixelValue.ToString());
+                            //MessageBox.Show(centerPixelValue.ToString());
+                            if(centerPixelValue > thresh)
+                            {
+                                //veci od thresholda
+                                bcenterPixelMax = true;
+
+                                for(int j =y-windowRad; j<= y+windowRad; j++)
+                                {
+                                    for (int i = x - windowRad; i <= x + windowRad; i++)
+                                    {
+                                        currentPixelValue = res.Data[j, i, 0];
+
+                                        //ako je trenutni veci, nije slovo
+                                        if (currentPixelValue > centerPixelValue)
+                                        {
+                                            bcenterPixelMax = false;
+                                            break; 
+                                        }
+                                    }
+                                    if (!bcenterPixelMax)
+                                        break;  //exit j
+                                }
+
+                                //nacrtaj pravokutnik oko slova
+                                if (bcenterPixelMax)
+                                {
+                                   // MessageBox.Show("ima podudaranje!");
+                                    draw = true;
+                                    rect_letter = new System.Drawing.Rectangle(x, y, templ.Width, templ.Height);
+                                     //this.pictureBox1.Invalidate();
+
+                                    System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Green, 2);
+                                    //  e.Graphics.DrawRectangle(pen, rect_letter);
+                                    Bgr color = new Bgr(0, 255, 0);
+                                    myImage.Draw(rect_letter, color, 2);
+
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+                templateFlag = false;
+                draw = false;
+                viewer.Image = myImage;
+                 viewer.ShowDialog();
+            }
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CaptureButton_Click(object sender, EventArgs e)
+        {
+            //uslikaj i prikazi sliku
+            Image<Bgr, double> capturedImage = cap.QueryFrame().ToImage<Bgr, double>();
+            ImageViewer viewer = new ImageViewer();
+            cap.Stop();
+            pictureBox1.Image = capturedImage.ToBitmap();
+            //viewer.Image = capturedImage;
+            // viewer.ShowDialog();
+
+
+
+
+            //Bitmap capturedImage_bm = new Bitmap(capturedImage);
+            //  Bitmap myImage = new Bitmap(pictureBox1.Image);
+            // Image<Bgr, byte> outImage = new Image<Bgr, byte>(myImage);
+
+
+
+
+            // CvInvoke.Canny(capturedImage,outImage, 100,100);
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void TemplateButton_Click(object sender, EventArgs e)
+        {
+            
+
+            if(methodBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Odaberi metodu!");
+            }
+            else
+            {
+                double outD;
+                if(double.TryParse(threshBox.Text.ToString(), out outD) ==false)
+                {
+                    MessageBox.Show("Ne ispravan broj!");
+                }
+                else
+                {
+                    string selectedItem = methodBox.Items[methodBox.SelectedIndex].ToString();
+                    // label2.Text = selectedItem;
+                    templateFlag = true;
+                }
+
+
+               
+            }
+            
+
+        }
+    }
+}
